c9cc2d6e5c741bfc794639f3d03728c06442e812
LSM <dlthdal07@gmail.com>
2024-10-10 09:33:02 +0900

    if(n < 0) 복구

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // computes a factorial of a given positive integer
 // version 1 -> 2 -> 3
 function factorial(n) {
-/*
+
     if( n < 0 ) {
         throw new Error('n must be a positive integer');
     }
-*/
+
 
     if( n === 0 ) {
         return 1;
